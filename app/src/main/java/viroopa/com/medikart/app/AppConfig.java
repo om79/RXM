5c1f6c22f78ece37d41af0611ad6bf317f96cf2b
OrderGeine <waseem.ahmed@ordergeine.co.in>
2018-09-17 16:47:35 +0530

    Check alies name

--- app/src/main/java/viroopa/com/medikart/app/AppConfig.java
+++ app/src/main/java/viroopa/com/medikart/app/AppConfig.java
@@ -1,7 +1,7 @@
 package viroopa.com.medikart.app;
 
 public class AppConfig {
-//Waseem commited for test 3
+//Waseem commited for
     public static final String IMAGE_DIRECTORY_NAME = "Medikart";
 
     //public static String URL_IPADDRESS = "http://198.50.198.184:86";
